a0089066b42248d69f5307ed454ca26733ee2181
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-12-30 21:59:59 +0900

    [Remove] /drafts/ ページへのリンクのhttps化処理を削除
    全ページhttpsになったため

--- chrome-extension/js/drafts_link_to_https.js
+++
@@ -1,5 +0,0 @@
-Array.from(document.getElementsByTagName('a')).forEach(anchor => {
-	if (anchor.href.startsWith('http://qiita.com/drafts/')) {
-		anchor.href = anchor.href.replace(/^http:/, 'https:');
-	}
-});
